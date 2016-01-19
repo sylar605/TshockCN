95197c3dcf13da1cd9c0286484f6dcb59b0d5e7c
Sylar <i@5dsy.info>
2016-01-20 01:50:26 +0800

    Revert "Revert "更改默认地图存放路径为服务端根目录Worlds""
    This reverts commit 48d77c83a17ec1f6fd7e4733044074333db7d843.

--- TerrariaServerAPI/Terraria/Main.cs
+++ TerrariaServerAPI/Terraria/Main.cs
@@ -2229,7 +2229,7 @@ namespace Terraria
 			Main.PendingPlayer = null;
 			Main.WorldList = new List<WorldFileData>();
 			Main.ActiveWorldFileData = new WorldFileData();
-			object[] objArray = new object[] { Environment.GetFolderPath(Environment.SpecialFolder.Personal), Path.DirectorySeparatorChar, "My Games", Path.DirectorySeparatorChar, "Terraria" };
+			object[] objArray = new object[] { "./" };
 			Main.SavePath = string.Concat(objArray);
 			Main.WorldPath = string.Concat(Main.SavePath, Path.DirectorySeparatorChar, "Worlds");
 			Main.CloudWorldPath = "worlds";
